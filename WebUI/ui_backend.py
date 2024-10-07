2f35b0aa2d997ed66f3db41ba374b7550ba26dbd
Listi1 <113858974+Listi1@users.noreply.github.com>
2024-10-07 10:14:20 +0200

    Add files via upload

---
+++ WebUI/ui_backend.py
@@ -0,0 +1,7 @@
+def send_iso(fileName): #vet ej om denna behövs eller kan köras direkt via TSK
+    print("Test")
+    return 0
+
+def send_csv(fileName):  #vet ej om denna behövs eller kan köras direkt via OLLAMA
+    print(fileName)
+    return 0
--- WebUI/ui_host.py
+++ WebUI/ui_host.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, jsonify
+from ui_backend import send_iso, send_csv
 
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
+    send_iso()
     return render_template('index.html')
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
+
 
 # Route to handle file name submission via AJAX
 @app.route('/submit-file', methods=['POST'])
@@ -16,9 +20,19 @@ def submit_file():
     file_name = request.form.get('filename')  # Get the file name from the AJAX request
     print(f"Received file name: {file_name}")
     
-    # You can now store the file name in a variable or process it as needed
+    if(file_name.endswith('.csv')):
+        send_csv(file_name) #ADD HANDLING FOR CSV
+    elif(file_name.endswith('.iso')):
+        send_iso(file_name) #ADD HANDLING FOR ISO
+    else:
+        print("non valid file type")
+
     # Return a response to the client
     return jsonify({"status": "success", "filename": file_name})
 
+def main():
+    app.run(threaded=True, port=4987)
+    return 0
+
 if __name__ == '__main__':
-    app.run(threaded=True, port=4994)
+    main()

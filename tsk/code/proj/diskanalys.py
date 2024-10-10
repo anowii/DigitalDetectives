abca3053d37f9be7ed1fcbc2bebe5dae55d2ef0d
B4lkongen <134702771+B4lkongen@users.noreply.github.com>
2024-10-10 09:35:52 +0200

    Add files via upload

--- tsk/code/proj/diskanalys.py
+++ tsk/code/proj/diskanalys.py
@@ -5,11 +5,13 @@
 import subprocess
 import sqlite3
 import csv
+import re
 
-TMP_DIR = "tmp_disk_analys/"
-DATA_DIR = "/home/nalle/Documents/proj/data/"
-SLEUTH_DB = "/home/nalle/Documents/proj/data/analys.db"
-CSV_PATH = "/home/nalle/Documents/proj/data/db.csv"
+DATA_DIR = "data/"
+SLEUTH_DB = "data/analys.db"
+CSV_PATH = "data/db.csv"
+
+INTERESTING_TYPES =".exe|.msi|.docx|.png|.mp4|.jpeg"
 
 def connect():
     con = sqlite3.connect(SLEUTH_DB)
@@ -32,12 +34,13 @@ def db_to_csv():
     res = cur.execute("SELECT * FROM tsk_files")
     db_files = res.fetchall()
     print("\ntsk_files\n")
-    fields = ["obj_id", "name", "meta_addr", "dir_flags" ,"size", "ctime", "crtime", "md5", "parent_path"]
+    fields = ["meta_addr", "name", "dir_type", "size", "crtime", "parent_path","md5"]
     files = []
     for file in db_files:
-        files.append({"obj_id":file[0], "name":file[5], "meta_addr":file[6], "dir_flags": file[12],"size":file[14], "ctime":file[15], "crtime":file[16], "md5":file[23], "parent_path":file[25]})
+        name = file[5]
+        if re.search(INTERESTING_TYPES,file[5]):
+            files.append({"meta_addr":file[6], "name":file[5], "dir_type": file[12], "size":file[14], "crtime":file[16], "parent_path":file[25],"md5":file[23]})
         
-    
     with open(CSV_PATH, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
@@ -47,10 +50,7 @@ def db_to_csv():
 def run(disk_image_path):
     
     check_input_file(disk_image_path)
-    subprocess.run(["mkdir", TMP_DIR], text=True)
     run_sleuth_on_file(disk_image_path)
     db_to_csv()
-    subprocess.run(["rm","-r", TMP_DIR], text=True)
-    return CSV_PATH
 
-run("/home/nalle/Documents/diskanalys/data/2020JimmyWilson.dd")
+    return CSV_PATH

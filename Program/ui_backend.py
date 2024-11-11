a95efa07f6ff6ce9d66621adf544b8431cb7c1bf
William Knutsson <william.knutsson@gmail.com>
2024-11-11 17:59:19 +0100

    sql agent

--- Program/ui_backend.py
+++ Program/ui_backend.py
@@ -2,13 +2,13 @@ import json
 import os
 import pandas as pd
 from langchain_ollama import OllamaLLM
-from templates import json_template, simple_template
+from templates import json_template, simple_template, sql_template
 from diskanalys import run
 
 model = OllamaLLM(model="llama3.2")
 #chain = json_template | model
 chain = simple_template | model
-
+chain_sql = sql_template | model
 
 # Load CSV data from filename/filepath returns it in JSON format 
 def load_json_data(filename):
@@ -31,18 +31,33 @@ def load_json_data(filename):
         print("File does not exist.")
         return []
 
+def send_query_to_db(query):
+    success = True
+    response = query
+    return response, success
 
 # Function to handle message forwarding to LLM
 def forward_message_llm(message, filepath):
+    # Load previous conversation history
 
-    # Load JSON data from 'current csv file'
-    json_data = load_json_data(filepath) 
-    print(f"JSON Data: {json_data}")    # Print for debug/checking purposes only
-  
-    # Use Langchain chain to get AI response
-    result = chain.invoke({"json_data": json_data ,"question": message})
+    json_data = load_json_data(filepath)  # Initialize JSON data as empty
+    print(f"JSON Data: {json_data}")  # Log the JSON data
+
+    # Determine weather to invoke sql agent or json chain (determining this using llm is too much trouble for what its worth)
+    if message.lower().startswith("list"):
+        print("List detected in question running sql agent")
+        query = chain_sql.invoke({"question": message})
+        result, sql_success = send_query_to_db(query)
+
+    # If sql query fails or it dosn't start with "list" answer the question normally with json data
+    if sql_success == False or message.lower().startswith("list") == False:
+        result = chain.invoke({"json_data": json_data ,"question": message})
     
-    return result  # Return the result to be used in Flask
+    # Use Langchain chain to get AI response
+    
+    #print('bye')
+    
+    return result
 
 def send_iso(fileName): #Runs TSK on disk image file (".dd")
     run(fileName)

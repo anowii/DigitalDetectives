5341eac9fe013a296bcb8d2161f3a92c2a2b3d6e
William Knutsson <william.knutsson@gmail.com>
2024-11-29 17:02:40 +0100

    Brain Button fetches query
    from corresponding AI-response

--- Program/static/context_btn.js
+++ Program/static/context_btn.js
@@ -1,15 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     $('#chat-window').on('click', '.context-btn', function () {
-        console.log('Button clicked:', $(this).attr('id')); // Debugging
         
         // Check if the clicked button is already active
         if ($(this).hasClass('active')) {
             // If it's active, deactivate it
             $(this).removeClass('active');
+            // Call python function to reset json mode(bool) to default
         } else {
             // If it's not active, deactivate all buttons and activate the clicked one
             $('.context-btn').removeClass('active');
             $(this).addClass('active');
+
+
+            // Fetch Query of message the button is attatched to
+            var buttonId = $(this).attr('id').replace('context-btn-', '');
+            var aiMessageElement = $('#ai-message-' + buttonId);
+            var aiMessageText = aiMessageElement.text();
+            var start = -1;
+
+            start = aiMessageText.indexOf("SELECT")
+            end = aiMessageText.indexOf(":name", start)
+            var query = aiMessageText.slice(start, end)
+            
+            console.log('Button clicked:', $(this).attr('id'), query);
+            if (start !== -1){
+                // Call python function to create json from query here
+                // Call python function to set json mode (bool) to "context" or something similar
+            }else{
+                console.log('No Query found');
+            }
+
         }
     });
 });
